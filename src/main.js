b9270198912127326e737b374bd3ad7ab7b6e87f
jemiluv8 <jemiluv8@gmail.com>
2026-01-21 02:37:26 +0000

    remove premature vue router thing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { initFoundation } from 'foundation-sdk/vue'
-
-// Initialize Foundation SDK with vue-router for direct navigation
-initFoundation({ router })
 
 createApp(App).use(router).mount('#app')
